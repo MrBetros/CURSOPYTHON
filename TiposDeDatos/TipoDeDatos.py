bd6ceb3801fcb5bbd1874a2b993bce1f5aaec024
Mario Alberto <zeroline1998@gmail.com>
2021-02-09 20:44:28 -0600

    TiposDatosAct3
    True

--- TiposDeDatos/TipoDeDatos.py
+++ TiposDeDatos/TipoDeDatos.py
@@ -4,12 +4,14 @@ Edad = 22;
 Sexo = "Masculino";
 Telefono = "5513387008";
 Peso = 73.5;
+LeGustaDormir = True;
 #str nos sirve para castear el dato colocado
 print("Nombre: " + NombrePersona);
 print("Edad: " + str(Edad));
 print("Sexo: " + Sexo);
 print("Telefono: " + Telefono);
 print("Peso: " + str(Peso));
+print("Le Gusta Dormir: " + str(LeGustaDormir));
 print("\n");
 print(type(NombrePersona));
 print(type(Edad));
