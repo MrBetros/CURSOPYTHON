648e656b45dbaa295663ceade97639dd545357b5
Mario Alberto <zeroline1998@gmail.com>
2021-02-09 20:40:29 -0600

    TiposDatosAct2
    Type

--- TiposDeDatos/TipoDeDatos.py
+++ TiposDeDatos/TipoDeDatos.py
@@ -3,9 +3,16 @@ NombrePersona = "Mario";
 Edad = 22;
 Sexo = "Masculino";
 Telefono = "5513387008";
+Peso = 73.5;
 #str nos sirve para castear el dato colocado
 print("Nombre: " + NombrePersona);
 print("Edad: " + str(Edad));
 print("Sexo: " + Sexo);
 print("Telefono: " + Telefono);
+print("Peso: " + str(Peso));
+print("\n");
+print(type(NombrePersona));
+print(type(Edad));
+print(type(Peso));
+
 
