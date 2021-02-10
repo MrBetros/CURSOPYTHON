ba28fa1582fa65ffcc4629d3aee5936dd789c9a2
Mario Alberto <zeroline1998@gmail.com>
2021-02-09 20:30:19 -0600

    TiposdeDatosActualizacion
    Datos Dinamicos

---
+++ TiposDeDatos/TipoDeDatos.py
@@ -0,0 +1,11 @@
+# Tipos De Datos
+NombrePersona = "Mario";
+Edad = 22;
+Sexo = "Masculino";
+Telefono = "5513387008";
+#str nos sirve para castear el dato colocado
+print("Nombre: " + NombrePersona);
+print("Edad: " + str(Edad));
+print("Sexo: " + Sexo);
+print("Telefono: " + Telefono);
+
