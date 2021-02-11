741b9cfe2850206f51f619597b8f588d42819caa
Mario Alberto <zeroline1998@gmail.com>
2021-02-10 21:08:23 -0600

    Condicionales

---
+++ Condicionales/Condicionales.py
@@ -0,0 +1,11 @@
+# Decir si es mayor de edad
+
+edad = input("Escribe tu edad: ");
+
+edad = int(edad);
+
+if (edad >= 18):
+    print("Mayor de Edad");
+elif (edad < 18):
+    print("Menor de Edad");
+
