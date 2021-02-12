73741aa5adfa9481e4c347c14403e1aa3c2baed0
Mario Alberto <zeroline1998@gmail.com>
2021-02-11 21:07:51 -0600

    Ejercicio2

--- Diccionarios/Diccionarios.py
+++ Diccionarios/Diccionarios.py
@@ -1,7 +1,13 @@
 #Respetar sintaxis
-diccionario = { "Nombres": "Mario Alberto", "Correo Electronico": "zeroline1998@gmail.com", "Telefono": 5513387008, "Apellidos": "García Rodríguez" };
+diccionario = { "Nombres": "Mario Alberto", "Correo Electronico": "zeroline1998@gmail.com", 
+"Telefono": 5513387008, "Apellidos": "García Rodríguez", 
+"Direccion": { "Calle": "Excursionistas Everest", "Numero": "M-229 L-2280" } };
 
 print(diccionario["Nombres"]);
 print(diccionario["Apellidos"]);
+print(diccionario["Direccion"]);
 print(diccionario["Correo Electronico"]);
 print(diccionario["Telefono"]);
+
+print("Claves(Keys):", diccionario.keys());
+print("Valores(Values):", diccionario.values());
---
+++ Ejercicios/Ejercicio2.py
@@ -0,0 +1,7 @@
+radio = input("Escribe el radio del circulo: ");
+pi = 3.1416;
+area = float(pi) * float(radio) ** float(2);
+perimetro = float(pi) ** float(2) * float(radio);
+
+print("El area del circulo es: " + str(area));
+print("El perimetro del circulo es: " +  str(perimetro));
