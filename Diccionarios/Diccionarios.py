ad034cffc0df16d2f5d749c49e6dd8d4bfaba3d1
Mario Alberto <zeroline1998@gmail.com>
2021-02-11 20:04:03 -0600

    Diccionarios

---
+++ Diccionarios/Diccionarios.py
@@ -0,0 +1,7 @@
+#Respetar sintaxis
+diccionario = { "Nombres": "Mario Alberto", "Correo Electronico": "zeroline1998@gmail.com", "Telefono": 5513387008, "Apellidos": "García Rodríguez" };
+
+print(diccionario["Nombres"]);
+print(diccionario["Apellidos"]);
+print(diccionario["Correo Electronico"]);
+print(diccionario["Telefono"]);
