cfc2b239af2045073a8d854cd5e2a689a1f09a5f
Mario Alberto <zeroline1998@gmail.com>
2021-02-12 14:52:20 -0600

    E4yS4

---
+++ Ejercicios/Ejercicio4.py
@@ -0,0 +1,11 @@
+num = input("Introduce un número: ");
+
+num = int(num);
+
+# El operador == evalua que los valores sean iguales para varios tipos de datos.
+if num == 0:
+    print ("Numero Par.");
+elif num%2 == 0:
+    print ("Numero Par.");
+else:
+    print ("Numero Impar.");
