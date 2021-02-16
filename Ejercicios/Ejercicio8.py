a8ad529bd926dc8324810fd82382518a5cb38cf6
Mario Alberto <zeroline1998@gmail.com>
2021-02-15 20:22:30 -0600

    CorreccionE8

--- Ejercicios/Ejercicio8.py
+++ Ejercicios/Ejercicio8.py
@@ -1,13 +1,12 @@
 rango = int(input("Coloca un numero: "));
 
-if rango < 0 and rango > -100:
-    for rango in range(-101, 0):
-        if rango % 2 != 0:
-            print(rango)
+if rango < 0 and rango > -101:
+    for rango in range(-1, -100, -2):
+        print(rango);
 elif rango > 0 and rango < 101:
     for rango in range(0, 101):
         if rango % 2 ==0:
-            print(rango)
+            print(rango);
 else:
     print("No valido.");
 
