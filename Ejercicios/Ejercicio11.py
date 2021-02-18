e1db50c4d0ea1077c4044aa342c75a4f44d8eb1e
Mario Alberto <zeroline1998@gmail.com>
2021-02-17 20:56:53 -0600

    ActualizacionE11

--- Ejercicios/Ejercicio11.py
+++ Ejercicios/Ejercicio11.py
@@ -1,6 +1,8 @@
-def Lista1(lista):
+def Lista1(lista, num):
     for item in lista:
-        print(item)
+        if item > num:   #Filtro   
+            print(item)
 
+num = 5 
 lista = [0,1,2,3,4,5,6,7,8,9]
-Lista1(lista)
+Lista1(lista, num)
