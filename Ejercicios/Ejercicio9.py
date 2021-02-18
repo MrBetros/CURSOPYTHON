f0fa38c563331ff5570bd6681733dd660cb93896
Mario Alberto <zeroline1998@gmail.com>
2021-02-17 19:42:52 -0600

    ActualizacionE9yS9

--- Ejercicios/Ejercicio9.py
+++ Ejercicios/Ejercicio9.py
@@ -1,12 +1,14 @@
-dias = float(input("Dias trabajados: "));
-horas = input("¿Trabajo horas extras? ");
-nomina = (dias * 250) ;
-prestaciones = nomina * float(.10);
-nominaff = nomina - prestaciones;
-if horas == 'si':
-    extra = float(input("¿Cuantas horas extras trabajaste? "))
-    final = extra * 31.25  
-    nominaf = ((dias * 250) + float(final)) - float(prestaciones);
-    print("Su nomina final es de: " + str(nominaf))
-else:
-    print("Su nomina es de: " + str(nominaff));
+dias = int(input("¿Cuantos dias trabajaste?: "));
+pagoxd = 250;
+pagob = dias * pagoxd;
+ivat = pagob * float(.16);
+sub = pagob + ivat;
+ivar = ivat * (2/3);
+ISRr = pagob * float(.10);
+pagon = (float(pagob) + float(ivat)) - (float(ivar) + float(ISRr));
+print("Su pago bruto es de: " + str(pagob));
+print("El IVA transladado es de: " + str(ivat));
+print("Subtotal: " + str(sub));
+print("El IVA retenido es de: " + str(ivar));
+print("El ISR retenido es de: " + str(ISRr));
+print("Su pago neto es de: " + str(pagon));
